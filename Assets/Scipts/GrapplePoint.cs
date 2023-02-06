d2daf2e89a7348b898b82d39732624977ba78243
MicahLawson <micah.a.lawson@students.pcci.edu>
2023-02-06 14:30:12 -0600

    Lasso WIP

---
+++ Assets/Scipts/GrapplePoint.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplePoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   // Is ran when the grapple is first detected
+   void onEnter()
+   {
+
+   }
+   
+   // Is ran when the grapple is no longer detected
+   void onLeave()
+   {
+
+   }
+      
+   // Called when the player presses the interact key
+   void grapple(Player player)
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+    {
+        
+    }
+}
