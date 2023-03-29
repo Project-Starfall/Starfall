cd6499c6a19b28b67c526e2e840bc84cc9732f61
MicahLawson <micah.a.lawson@students.pcci.edu>
2023-03-29 15:50:06 -0500

    Fixes fishing line width

--- Assets/Scipts/Puzzles/FishingGame/FishingGameMovement.cs
+++ Assets/Scipts/Puzzles/FishingGame/FishingGameMovement.cs
@@ -26,6 +26,7 @@ public class FishingGameMovement : MonoBehaviour
     [SerializeField] private GameObject hookConnection; // Where the line connects to the hook
     [SerializeField] private SpriteRenderer hookedFishSprite; // Fish sprite from on the fishing hook
     private Color lineColor = Color.white; // Color of the fishing line
+    [SerializeField] private Material lineSpriteMaterial; // Material of the fishing material
     [SerializeField] private SpriteRenderer movingFishSprite; // Fish sprite from bottom of water
     [SerializeField] private GameObject rodConnection; // Where the line connects to the rod
     private int trashLayer; // Int value of the "Trash" layer mask
@@ -44,11 +45,13 @@ public class FishingGameMovement : MonoBehaviour
     void Start()
     {
         LineRenderer line = gameObject.AddComponent<LineRenderer>();
-        line.widthMultiplier = 0.05f;
         line.positionCount = 2;
         line.sortingOrder = 4;
         Material lineMaterial = GetComponent<LineRenderer>().material;
+        lineMaterial = lineSpriteMaterial;
         lineMaterial.color = lineColor;
+        line.material = lineMaterial;
+        line.widthMultiplier = 0.05f;
         line.sortingLayerName = "UI";
 
         hookCollider = hook.GetComponent<CircleCollider2D>();
