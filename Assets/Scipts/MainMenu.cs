e16dca4691c6dc038deb1e91eb8b15b6cf8145f1
Dapo Adegunloye <dapoadegunloye123@gmail.com>
2023-04-24 14:54:30 -0500

    menu
    removed the load button and add the game name to both menu

--- Assets/Scipts/MainMenu.cs
+++ Assets/Scipts/MainMenu.cs
@@ -15,32 +15,7 @@ public class MainMenu : MonoBehaviour
     public GameObject LoadButtonUI;
 
     [SerializeField] PlayableDirector newGameStart;
-
-    // Load button fields
-    [SerializeField]
-    Button loadButton;
-    [SerializeField]
-    Image loadBkgrdImage;
-    [SerializeField]
-    TMP_Text loadButtonTxt;
-
-
-    // Start
-    public void Start()
-    {
-        if (!saveExist())
-        {
-            loadButton.enabled = false;
-            loadBkgrdImage.enabled = false;
-            loadButtonTxt.faceColor = new Color(156,156,156);
-        }
-        else
-        {
-            loadButton.enabled = true;
-            loadBkgrdImage.enabled = true;
-            loadButtonTxt.faceColor = new Color(255, 255, 255);
-        }
-    }
+    
     /* is there a save -> set load button active
      * if not -> disable load button
      * 
@@ -75,25 +50,6 @@ public class MainMenu : MonoBehaviour
      * but if dont have a save
      * create a new save | newSave()
      */
-
-    public void LoadGameMenu()
-    {
-        PlayerData playerData;
-        // This function Loads a saved game
-        if(saveExist())
-        {
-            LoadButtonUI.SetActive(true);
-            playerData = LoadGame();
-            // SaveSystem.LoadGame(SceneManager.GetActiveScene().path);
-            // load scene ()
-            SceneManager.LoadScene(playerData.level);
-        }
-        else
-        {
-            LoadButtonUI.SetActive(false);
-            Debug.Log("No saved game found.");
-        }
-    }
 
     public void playTutorial()
     {
